d470fd38d06a7ee2c40ccdc15426d77a74bc1abd
Anurag Pramod Shingare <anuragshingare8@gmail.com>
2024-12-10 20:52:07 +0530

    client and server

--- Hardhat_ToDo/hardhat_server/contracts/ToDo.sol
+++ Hardhat_ToDo/hardhat_server/contracts/ToDo.sol
@@ -1,7 +1,41 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 
 contract ToDo {
-    
-}
+    struct AllTasks {
+      uint id;  
+      string task;
+      bool isDeleted;
+    }
+
+    mapping (address => AllTasks[]) public tasks;
+
+
+    event AddedTask(address owner, string taskAdded);
+    event DeletedTask(address owner, string taskDeleted);
+
+
+    // function to add task
+    function addTask(string memory addtask) public {
+        AllTasks memory newTask =  AllTasks({
+            id:tasks[msg.sender].length,
+            task:addtask,
+            isDeleted:false
+        });
+        tasks[msg.sender].push(newTask);
+        emit AddedTask(msg.sender,addtask);
+    }
+
+    // get all tasks
+    function getAllTask() public view returns(AllTasks[] memory){
+        require(tasks[msg.sender].length != 0, "List is empty");
+        return tasks[msg.sender];
+    }
+
+    // function to delete task
+    function deleteTask(uint _id) public{
+        tasks[msg.sender][_id].isDeleted = true;
+        emit DeletedTask(msg.sender, tasks[msg.sender][_id].task);
+    }
+}   
--- Hardhat_ToDo/hardhat_server/hardhat.config.js
+++ Hardhat_ToDo/hardhat_server/hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
+const { vars } = require("hardhat/config");
+
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const ETHERSCAN_ID = vars.get("ETHERSCAN_ID");
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.27",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_ID,
+  },
+  sourcify: {
+    enabled: true,
+  },
 };
