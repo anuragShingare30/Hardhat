98369be8108980509f6b1b66e2d3391fef0603bd
Anurag Pramod Shingare <anuragshingare8@gmail.com>
2025-01-10 02:05:33 +0530

    Venmo frontend

--- hardhat_Venmo/frontend/my-react-app/src/App.tsx
+++ hardhat_Venmo/frontend/my-react-app/src/App.tsx
@@ -1,59 +1,95 @@
-import { useState,useEffect } from "react"
-import { Web3 } from 'web3';
+import { useState, useEffect } from "react";
+import { Web3 } from "web3";
+import { contractAddress } from "./config";
+import contractAbi from "../../../artifacts/contracts/Venmo.sol/Venmo.json";
+
+import { useForm } from "react-hook-form";
 
 export default function App() {
   // state varibale
-  const [account, setAccount] = useState<string|undefined>("");
+  const [account, setAccount] = useState<string | undefined>("");
   const [correctNet, setCorrectNet] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false);
 
   // create web3 and contract instance
-  const {ethereum} = window;
+  const { ethereum } = window;
   const web3 = new Web3(ethereum);
+  const abi = contractAbi.abi;
+  const venmo = new web3.eth.Contract(abi, contractAddress);
 
+  // creating form instance
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm();
 
   // connect wallet function
-  const ConnectWalletBtn = async()=>{
+  const ConnectWalletBtn = async () => {
     try {
-      if(!ethereum){
-        return <h1>Metamask wallet not found!!!</h1>
-    }
+      if (!ethereum) {
+        return <h1>Metamask wallet not found!!!</h1>;
+      }
 
-    // get chain-id
-    const chainID = await ethereum.request({method:"eth_chainId"});
-    console.log(chainID);
+      // get chain-id
+      const chainID = await ethereum.request({ method: "eth_chainId" });
+      console.log(chainID);
 
-    // check for correct network
-    const sepolia_chain_id = "0xaa36a7"; // 11155111
-    if(chainID != sepolia_chain_id){
-      alert("You are not connected to sepolia!!");
-      return;
-    }
-    setCorrectNet(true);
+      // check for correct network(sepolia)
+      // const sepolia_chain_id = "0xaa36a7"; // 11155111
+      // if(chainID != sepolia_chain_id){
+      //   alert("You are not connected to sepolia!!");
+      //   return;
+      // }
+      // setCorrectNet(true);
 
-    // get accounts
-    const accounts = await ethereum.request({method:"eth_requestAccounts"});
-    console.log(accounts[0]);
-    setAccount(accounts[0]);
-    setLoggedIn(true);
+      // check for correct network for testing purpose(anvil)
+      const anvil_chain_id = "0x7a69"; // 31337
+      if (chainID != anvil_chain_id) {
+        alert("You are not connected to anvil!!");
+        return;
+      }
+      setCorrectNet(true);
 
-    // set function for account changed
-    ethereum.on('accountsChanged', async()=>{
-      const accounts = await web3.eth.getAccounts();
+      // get accounts
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log(accounts[0]);
       setAccount(accounts[0]);
-    })
+      setLoggedIn(true);
 
+      // set function for account changed
+      ethereum.on("accountsChanged", async () => {
+        const accounts = await web3.eth.getAccounts();
+        setAccount(accounts[0]);
+      });
     } catch (error) {
       console.log(error);
     }
-  }
+  };
+
+  // sending ethers and message to user
+  const sendEther = async (data) => {
+    try {
+      if(!ethereum){
+        alert("No wallet detected!!!");
+        return;
+      }
+      await venmo.methods.sendEthers(1000000, data.toAddress,data.message).send({
+        from:account,
+        value:web3.utils.toWei("0.0001","ether"),
+        gas:"300000",
+        gasPrice:undefined
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    ConnectWalletBtn();
-  }, [])
+    // ConnectWalletBtn();
+  }, []);
 
   return (
     <div>
+      {/* connect wallet btn */}
       <div className="bg-base-300 flex flex-row items-center justify-around p-4">
         <h1 className="text-3xl font-bold text-violet-600">Venmo</h1>
         {/* <div className="text-black btn btn-sm btn-accent">0x09jhfbfhbfhfbfhfbr7383903bffgfbfhf</div> */}
@@ -61,17 +97,40 @@ export default function App() {
           <div>
             <h1 className="text-black btn btn-sm btn-accent">{account}</h1>
           </div>
-        ):(
-            <button
-                className="text-black btn btn-sm btn-accent"
-                onClick={ConnectWalletBtn}
-            >
-              Connect Wallet
-            </button>
+        ) : (
+          <button
+            className="text-black btn btn-sm btn-accent"
+            onClick={ConnectWalletBtn}
+          >
+            Connect Wallet
+          </button>
         )}
       </div>
+      {/* form */}
+      <div className="p-4 m-4">
+        <form onSubmit={handleSubmit(sendEther)}>
+
+          <input
+            placeholder="enter to address"
+            {...register("toAddress", { required: true, minLength: 42, maxLength: 42 })}
+            className="input input-primary"
+          />
+          {errors.toAddress?.type === 'required' && <p role="alert">Reciever address is required</p>}
+          <input
+            placeholder="enter the message"
+            {...register("message", { required: true, minLength: 6, maxLength: 20 })}
+            className="input input-primary"
+          />
+          {errors.message?.type === 'required' && <p role="alert">message is required</p>}
+          <button 
+              type="submit" 
+              className="btn btn-sm btn-accent" 
+              disabled={isSubmitting}
+              >
+            {isSubmitting ? "Loading..." : "Send"}
+          </button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
-
-
--- hardhat_Venmo/test/VenmoTest.ts
+++ hardhat_Venmo/test/VenmoTest.ts
@@ -2,8 +2,11 @@ import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
 import assert from "assert";
 import { ethers } from "hardhat";
+import Web3, { web3ProvidersMapUpdated } from "web3";
 
 describe("Lottery contract", function () {
+
+  const web3 = new Web3();
 
   // deploy contract instance / fixtures
   async function deployTokenFixture() {
@@ -20,6 +23,24 @@ describe("Lottery contract", function () {
   it("Initially the senders array is empty!!!", async function(){
       // for every test now we can use fixture instead of redepolying contract.
       const { venmo, owner } = await loadFixture(deployTokenFixture);
-      assert.equal(venmo.s_sendersInfo.length,0);
+      assert.equal(venmo.getAllSendersInfo.length,0);
+  })
+
+  // second test
+  it("After calling the sendEthers function, the array length should increase and balance of receiver should increase!!!", async function(){
+      const { venmo, owner, addr1} = await loadFixture(deployTokenFixture);
+
+      const amountToSend = web3.utils.toWei('0.001','ether');
+      const message = "Have some fun!!!";
+      const initialBalance = await web3.eth.getBalance(addr1.address);
+
+      await venmo.sendEthers(amountToSend,addr1.address,message,{value:amountToSend});
+
+      
+      
+      const afterTnxBalance = await web3.eth.getBalance(addr1.address); 
+      assert.equal(BigInt(initialBalance)+BigInt(amountToSend),(afterTnxBalance));
+      // assert.equal(await venmo.getAllSendersInfo.length,1);
+      
   })
 });
