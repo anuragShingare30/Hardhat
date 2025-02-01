2b8fc9b928549a95a287418d8297176cd4f0bb26
Anurag Pramod Shingare <anuragshingare8@gmail.com>
2025-02-01 11:14:22 +0530

    Completed DEX main contract

---
+++ hardhat_DEX/contracts/BaseToken.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BaseToken is ERC20, Ownable {
+    constructor()
+        ERC20("MyToken", "MTK")
+        Ownable(msg.sender)
+    {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+}
